515316c9a43b7249616e48ca59ffd2ab4c1a374a
dadolyner <skulj.david@gmail.com>
2022-06-13 11:08:36 +0200

    Backend - connection to firebase

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,28 @@ import Fastify from 'fastify';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { Logger } from '@nestjs/common';
 
+import * as Firebase from 'firebase-admin';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 const nextup = async () => {
+    try {
+        Firebase.initializeApp({
+            credential: Firebase.credential.cert({
+                privateKey: process.env.FIREBASE_PRIVATE_KEY,
+                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+                projectId: process.env.FIREBASE_PROJECT_ID,
+            } as Partial<Firebase.ServiceAccount> ),
+            databaseURL: process.env.FIREBASE_DATABASE_URL,
+        })
+        Logger.log('Application is successfully connected to Firebase', 'Firebase');
+    } catch (error) {
+        Logger.error(error);
+    }
+
     const fastify = Fastify();
     const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(fastify), { cors: true });
     await app.listen(3000);
-    Logger.log('Application is listening on port 3000', 'NextUp');
+    Logger.log('Application is listening on port http://localhost:3000', 'NextUp');
 }
 nextup();
