5f8cb8bf6d71a102ab20731f72189e9a7f8b713f
dadolyner <skulj.david@gmail.com>
2022-06-13 07:43:18 +0200

    change express for fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import Fastify from 'fastify';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { Logger } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const nextup = async () => {
+    const fastify = Fastify();
+    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(fastify), { cors: true });
+    await app.listen(3000);
+    Logger.log('Application is listening on port 3000', 'NextUp');
 }
-bootstrap();
+nextup();
