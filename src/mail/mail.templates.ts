0184a10ed9b7dfbcf6dc6e38255f79993bb9f2ee
dadolyner <skulj.david@gmail.com>
2022-06-16 11:06:43 +0200

    sending emails to everyone when event is added

--- src/mail/mail.templates.ts
+++ src/mail/mail.templates.ts
@@ -39,3 +39,51 @@ export const VerifyEmailTemplate = (first_name: string, last_name: string, verif
     `;
 }
 
+// User added event template
+export const CheckoutEventTemplate = (first_name: string, last_name: string, date: string, time: string, checkoutLink: string, userCreated: boolean) => {
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Forgot Password</title>
+        </head>
+        <body>
+            <table>
+                <tr>
+                    <td>
+                        <img src='https://i.postimg.cc/y8Y6Spbm/NextUp.png' border='0' alt='NextUp-Logo' width='20px' height='20px'/>
+                        <span style="color: #2F3C7E; font-size: 20px;">NextUp</span>
+                    </td>
+                </tr>
+
+                <tr><td><h3 style="font-size: 16px;"><span style="font-weight: 200;">Hey </span>${first_name} ${last_name}<span style="font-weight: 200;">,</span></h3></td></tr>
+
+                ${ userCreated ? `
+                    <tr><p style="margin:0;">You have successfully created a new event.</p></tr>
+                ` : `
+                    <tr><p style="margin:0;">An artist just added a new event that will be happening on <b>${date}</b> at <b>${time}</b></p></tr>
+                ` }
+
+                ${ userCreated ? `
+                    <tr><p style="margin:0;">To checkout details of your event click the link below.</p></tr>
+                ` : `
+                    <tr><p style="margin:0;">To checkout this event and secure your spot click the link below and attend this event.</p></tr>
+                ` }
+
+                <tr><td>&nbsp;</td></tr>
+
+                <tr><a style="color: #2F3C7E;" href="${checkoutLink}">Checkout Event</a></tr>
+
+                <tr><td>&nbsp;</td></tr>
+
+                <tr><p style="margin:0;">Best regards,</p></tr>
+
+                <tr><span style="color: #2F3C7E; font-size: 16px;">NextUp</span></tr>
+            </table>
+        </body>
+        </html>
+    `;
+}
--- src/modules/events/events.repository.ts
+++ src/modules/events/events.repository.ts
@@ -5,18 +5,23 @@ import { Events } from "src/entities/events.entoty";
 import { Users } from "src/entities/users.entity";
 import { CreateEventDTO } from "./dto/create-event.dto";
 import { v4 as uuid } from "uuid";
+import { CheckoutEventTemplate } from "src/mail/mail.templates";
+import SendEmail from "src/mail/mail.config";
 
 export class EventsRepository {
     private logger = new Logger('EventsRepository');
-    constructor(public eventsFirebase = getRepository(Events)) { }
+    constructor(
+        public eventsFirebase = getRepository(Events),
+        private usersFirebase = getRepository(Users)
+    ) { }
 
     // Get all events
     async getAllEvents(): Promise<Events[]> {
         try {
             const events = await this.eventsFirebase.find();
-            if(!events) this.logger.error(`No events found!`);
+            if (!events) this.logger.error(`No events found!`);
             else this.logger.verbose(`Successfully retrieved all ${events.length} events!`);
-            
+
             return events;
         } catch (error) {
             this.logger.error(`Failed to retrieve all events!`);
@@ -28,7 +33,7 @@ export class EventsRepository {
     async getEventById(eventId: string): Promise<Events> {
         try {
             const event = await this.eventsFirebase.whereEqualTo('id', eventId).findOne();
-            if(!event) this.logger.error(`Event with id: ${eventId} does not exist!`);
+            if (!event) this.logger.error(`Event with id: ${eventId} does not exist!`);
             else this.logger.verbose(`Successfully retrieved details for event with id ${eventId}!`);
 
             return event;
@@ -53,23 +58,53 @@ export class EventsRepository {
         event.image = image
         event.userId = id
 
+        const getAllUsers = await this.usersFirebase.find()
         const eventExists = await this.eventsFirebase
-        .whereEqualTo('event_name', event_name)
-        .whereEqualTo('location', location)
-        .whereEqualTo('date', new Date(date).toISOString())
-        .find()
+            .whereEqualTo('event_name', event_name)
+            .whereEqualTo('location', location)
+            .whereEqualTo('date', new Date(date).toISOString())
+            .find()
 
-        if(eventExists.length > 0) {
+        if (eventExists.length > 0) {
             this.logger.error(`User with email: ${email} tried to made an event but this event already exists!`);
             throw new ConflictException('Event like this already exists!');
         } else {
-            try { this.eventsFirebase.create(event) }
+            try {
+                await this.eventsFirebase.create(event)
+
+                if (getAllUsers.length > 0) {
+                    this.logger.verbose(`Opening connection to send mail to all users about event: ${event.id}!`);
+                    getAllUsers.forEach(async user => {
+                        const date = new Date(event.date).toLocaleDateString()
+                        const time = new Date(event.date).toLocaleTimeString().substr(0, 5)
+                        let emailData
+                        if (user.email === email) {
+                            emailData = {
+                                to: user.email,
+                                from: 'skulj.david@gmail.com',
+                                subject: `You just created an event!`,
+                                html: CheckoutEventTemplate(user.first_name, user.last_name, date, time, `${process.env.CHECKOUT_EVENT}/${event.id}`, true),
+                            }
+                        } else {
+                            emailData = {
+                                to: user.email,
+                                from: 'skulj.david@gmail.com',
+                                subject: `${first_name} ${last_name} just added a new event!`,
+                                html: CheckoutEventTemplate(user.first_name, user.last_name, date, time, `${process.env.CHECKOUT_EVENT}/${event.id}`, false),
+                            }
+                        }
+                        await SendEmail(emailData)
+                        this.logger.verbose(`User: ${user.first_name} ${user.last_name} successfully recieved mail about this event!`);
+                    })
+                    this.logger.verbose(`Closing connection. Mail successfully sent to all users!`);
+                }
+            }
             catch (error) {
                 this.logger.error(`Internal server error: ${error}`);
                 throw new InternalServerErrorException()
             }
         }
-            
+
         this.logger.verbose(`User: ${first_name} ${last_name} successfully created an event named: ${event_name}!`);
         return event;
     }
@@ -91,7 +126,7 @@ export class EventsRepository {
         event.max_users = max_users
         event.description = description
         event.image = image
-        
+
         try { this.eventsFirebase.update(event) }
         catch (error) {
             this.logger.error(`There has been an error trying to update event: ${eventId}`);
@@ -107,9 +142,9 @@ export class EventsRepository {
     async deleteEvent(eventId: string, user: Users): Promise<Events> {
         const { id, first_name, last_name } = user;
         const event = await this.eventsFirebase.whereEqualTo('id', eventId).whereEqualTo('userId', id).findOne();
-        if (!event) { 
+        if (!event) {
             this.logger.error(`User ${first_name} ${last_name} does not have an event with id: ${eventId}!`);
-            throw new InternalServerErrorException() 
+            throw new InternalServerErrorException()
         }
 
         try { this.eventsFirebase.delete(event.id) }
